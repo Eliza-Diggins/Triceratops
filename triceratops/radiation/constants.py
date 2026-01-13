49d7e3fa6d3b820490da7598f736373f93418747
Eliza Diggins <eliza.diggins@utah.edu>
2026-01-13 15:34:23 -0800

    feat: Added some new CGS constants.

--- triceratops/radiation/constants.py
+++ triceratops/radiation/constants.py
@@ -29,3 +29,7 @@ electron_rest_energy: u.Quantity = const.m_e * const.c**2
 r"""astropy.units.Quantity: Electron rest energy :math:`E_0`."""
 electron_rest_energy_cgs: float = electron_rest_energy.cgs.value
 """float: Electron rest energy :math:`E_0` in CGS units."""
+sigma_T_cgs: float = const.sigma_T.cgs.value
+r"""float: Thomson cross-section :math:`\sigma_T` in CGS units."""
+c_cgs: float = const.c.cgs.value
+"""float: Speed of light :math:`c` in CGS units."""
